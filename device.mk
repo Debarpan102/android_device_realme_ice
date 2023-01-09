d7ea565feda082d71e000f9b9dd7e3d753440947
orges <me@orgesified.ml>
2023-01-09 22:59:31 +0100

    ice: configs: Add display config file
     * DisplayPowerController[0]: Screen brightness nits configuration is unavailable; falling back
     * DisplayDeviceConfig: requesting nits when no mapping exists.
    
    Change-Id: I96331fb70c7f8c4bd6e630c9826465b232191fbe
    
    d

--- device.mk
+++ device.mk
@@ -34,6 +34,18 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
+# Camera
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/media/media_profiles_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_vendor.xml
+
+# Display Config
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/displayconfig/display_id_4630946741972277890.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946741972277890.xml
+
+# FaceSense
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.face.sense_service.camera_id=1
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
